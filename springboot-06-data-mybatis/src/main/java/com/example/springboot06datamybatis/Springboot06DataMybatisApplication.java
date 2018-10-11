d3ef21ee27544fc1dd29e6fbf88373b48a990f83
hyunbar777 <32158243+hyunbar777@users.noreply.github.com>
2018-10-11 16:26:01 +0800

    1

---
+++ springboot-06-data-mybatis/src/main/java/com/example/springboot06datamybatis/Springboot06DataMybatisApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot06datamybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot06DataMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot06DataMybatisApplication.class, args);
+    }
+}
